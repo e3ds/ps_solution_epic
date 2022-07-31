2a57a76f23d6125f3235bff2e358cf8782c516f7
AArc-tic-Wolf <aninda.messy@gmail.com>
2022-07-31 02:12:50 +0000

    setup for listening to port 5508 on pixelstreaming domain

--- cirrus-web-server/common2.js
+++ cirrus-web-server/common2.js
@@ -1,3 +1,3 @@
-const SERVER = "https://cirrus1.eagle3dstreaming.com";
+const SERVER = "https://cirrus2.eaglepixelstreaming.com";
 const MASTER = "https://login1.eagle3dstreaming.com";
 module.exports={SERVER,MASTER};
