b649d5cd5c4bd94b35a5bfdfd1cb6cd7db23686e
Arc-tic-Wolf <aninda.messy@gmail.com>
2022-09-20 00:01:20 +0600

    forgot to revert back

--- cirrus-web-server/public/js/sample.js
+++ cirrus-web-server/public/js/sample.js
@@ -4,6 +4,6 @@ var mail = document.currentScript.getAttribute("mail");
 console.log("logged in by "+username+" ("+mail+")");
 
 function logOff() {
-    console.log("logoff");
-    window.location.assign("https://login1.eagle3dstreaming.com/signin?uri="+base);
+
+    window.location.assign("/signoff?route="+window.location.pathname);
 }
