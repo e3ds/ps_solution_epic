9565731b202b26e4e79deede834862df1aade4b8
Arc-tic-Wolf <aninda.messy@gmail.com>
2022-09-19 22:39:15 +0600

    log off button with proper redirect

--- cirrus-web-server/public/js/sample.js
+++ cirrus-web-server/public/js/sample.js
@@ -2,3 +2,9 @@ var username = document.currentScript.getAttribute("username");
 var mail = document.currentScript.getAttribute("mail");
 
 console.log("logged in by "+username+" ("+mail+")");
+
+function logOff() {
+    let text= JSON.stringify({continueUrl:window.location.href , timestamp: Date.now() });
+    let base=btoa(text);
+    window.location.assign("https://login1.eagle3dstreaming.com/signin?uri="+base);
+}
