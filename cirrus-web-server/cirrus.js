6fd086485497b2933c6ad12dd7cc2a47096a4e67
Arc-tic-Wolf <aninda.messy@gmail.com>
2022-09-20 00:10:28 +0600

    missed a variable fresh

--- cirrus-web-server/cirrus.js
+++ cirrus-web-server/cirrus.js
@@ -279,8 +279,8 @@ app.get("/signoff",(req,res)=> {
 	req.session.username = undefined;
 	req.session.loggedIn = false;
 	let text= JSON.stringify({continueUrl:SERVER , timestamp: new Date().getTime() });
-	let base=Buffer.from(text).toString('base64');;
-	res.redirect(MASTER+'/signin?uri='+SERVER);
+	let base=Buffer.from(text).toString('base64');
+	res.redirect(MASTER+'/signin?uri='+base);
 });
 
 app.get('/clear',(req,res)=>{
